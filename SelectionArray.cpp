4f063860f42b4710be3a034d71d808b9f2d1223f
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-18 23:21:07 +0530

    [ADDED] : YOGESH Selection Sort DSA

---
+++ SelectionArray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	cout<<"Enter the size of Array :";
+	cin>>n;
+	
+	int arr[n];
+	
+	cout<<"Unsorted Array :";
+	for(int i=0 ; i<n ; i++)
+	{
+		cin>>arr[i];
+	}
+	
+	for(int i=0 ; i<n-1 ; i++)
+	{
+			for(int j=i+1 ; j<n ; j++)
+			{
+				if(arr[j] < arr[i])
+				{
+					int temp = arr[j];
+					arr[j] =  arr[i];
+					arr[i] = temp ;
+				}
+			}
+	}
+	
+	cout<<"Sorted Array :";
+	for(int i=0 ; i<n ; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	
+	
+}
