e72dcf9885936ec8899a200ddc6fb6f2c461b679
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-04-13 23:54:22 +0530

    [ADDED] : Yogesh - Why java not support Multiple Inheritanc

---
+++ DiamondProblemSolution.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+class A{
+	
+	public :
+		
+	virtual	void funA()
+	{
+		cout << "Function A";
+	}
+};
+
+
+class B : public A{
+	
+	public :
+	void funB()
+	{
+		cout << "Function B";
+	}
+};
+
+class C : public A{
+	
+	public :
+	void funC()
+	{
+		cout << "Function C";
+	}
+};
+
+class D : public C , public B{
+	
+	public :
+	void funD()
+	{
+		cout << "Function D";
+	}
+	
+};
+
+int main()
+{	
+	A *a;
+	
+	D d ;
+	
+	a = &d;
+	
+	a->funA();
+	
+	return 0;
+}
